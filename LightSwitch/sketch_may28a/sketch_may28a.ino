e4dc9f2e1dda29ab61359bffd0d1a074a95d62fe
Ryan Dalal <ryanadalal@gmail.com>
2022-05-28 18:57:47 -0400

    Add files via upload

---
+++ LightSwitch/sketch_may28a/sketch_may28a.ino
@@ -0,0 +1,31 @@
+#include <WiFi.h>
+
+#define CONNECTION_TIMEOUT 10
+
+const char* ssid = "Tumba_2G";
+const char* password = "Dalal123";
+
+void setup(){
+    Serial.begin(115200);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(ssid, password);
+    Serial.println("\nConnecting");
+    int timeout_counter = 0;
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(200);
+        timeout_counter++;
+        if(timeout_counter >= CONNECTION_TIMEOUT*5){
+        ESP.restart();
+        }
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop(){}
